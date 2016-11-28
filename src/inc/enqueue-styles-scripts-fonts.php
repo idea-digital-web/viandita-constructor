e6783f006aab191b7a1b621068f4f0197ce511d5
Alex Ballera <alexballera@gmail.com>
2016-11-28 15:51:35 -0400

    optimizando

--- src/inc/enqueue-styles-scripts-fonts.php
+++ src/inc/enqueue-styles-scripts-fonts.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * INSERTAR ARCHIVO CSS STOREFRONT, 
+ * INSERTAR ARCHIVO CSS STOREFRONT,
  * Y ARCHIVOS CSS Y JS COMO THEME CHILD
 
  * get_template_directory_uri will look up parent theme location
@@ -15,6 +15,7 @@
 // Agregar Vendors JS
 function add_vendors_js(){
         wp_enqueue_script('sticky', '//cdn.jsdelivr.net/stickynavbar.js/1.3.0/jquery.stickyNavbar.min.js', array(), '1', true );
+        // wp_enqueue_script('smoove', get_stylesheet_directory_uri(). '/js/vendors/jquery.smoove.js', array('jquery'), '1', true );
         wp_enqueue_script('easing', 'http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js', array(), '1', true );
         wp_enqueue_script('recaptcha', 'https://www.google.com/recaptcha/api.js', array(), '1' );
         // wp_enqueue_script('flexslider', get_stylesheet_directory_uri(). '/js/vendors/jquery.flexslider.js', array(), '1', true );
@@ -34,17 +35,17 @@ add_action('wp_enqueue_scripts', 'my_scripts', 11);
 
 // Agregar Mis Estilos (ya lo está agregando el tema padre)
 // function my_stylesheet() {
-//     wp_enqueue_style( 'myStyle', get_stylesheet_directory_uri().'/style.css', array('theme'), '1.0', 'screen, projection'); 
+//     wp_enqueue_style( 'myStyle', get_stylesheet_directory_uri().'/style.css', array('theme'), '1.0', 'screen, projection');
 // }
 // add_action( 'wp_enqueue_scripts', 'my_stylesheet' );
 
-// Agregar Fonts 
+// Agregar Fonts
 function add_fonts() {
 	wp_enqueue_style( 'asap', '//fonts.googleapis.com/css?family=Asap:400,400i,700,700i&subset=latin-ext', array( 'storefront-style' ) );
 }
 add_action( 'wp_enqueue_scripts', 'add_fonts' );
 
-// Agregar Vendors CSS 
+// Agregar Vendors CSS
 function add_vendors_css() {
 	wp_enqueue_style('animate', '//cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css', array('storefront-style'));
 }
--- src/templates/content/banners.php
+++ src/templates/content/banners.php
@@ -1,15 +1,15 @@
 <div class="content-area__slides">
-  <div>
+  <div id="banner-left" class="content-area__image content-area__image-left">
   <!-- <a href="<?php home_url(); get_template_part( 'templates/content/banner', 'url1'); ?>"> -->
     <img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-content1.jpg" alt="Banner Content" />
   <!-- </a> -->
   </div>
-  <div>
+  <div id="banner-center" class="content-area__image content-area__image-center">
     <!-- <a href="<?php home_url(); get_template_part( 'templates/content/banner', 'url2'); ?>"> -->
       <img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-content2.jpg" alt="Banner Content" />
     <!-- </a> -->
   </div>
-  <div>
+  <div id="banner-rigth" class="content-area__image content-area__image-rigth">
   <!-- <a href="<?php home_url(); get_template_part( 'templates/content/banner', 'url3'); ?>"> -->
     <img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner-content3.jpg" alt="Banner Content" />
   <!-- </a> -->
